a43af3a3f3af1e01531ce912d247102d80e94d0f
Tuhin <tohin@coderex.co>
2023-02-07 22:08:56 +0600

    feat: create protected routes

--- frontend/src/components/DefaultLayout.jsx
+++ frontend/src/components/DefaultLayout.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useStateContext } from '../contexts/ContextProvider'
 
 export default function DefaultLayout() {
+  const { token } = useStateContext();
+
+  if( !token ){
+    return <Navigate to="/login"/>
+  }
+
   return (
     <div>
       For logged in users only
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { ContextProvider } from './contexts/ContextProvider'
 import './index.css'
 import router from './routes/router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    < RouterProvider router={router} />
-  </React.StrictMode>,
+    <ContextProvider>
+      <RouterProvider router={router} />
+    </ContextProvider>
+  </React.StrictMode>
 )
