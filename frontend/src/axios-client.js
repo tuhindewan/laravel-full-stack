cc2346d226fcdcda2b54f4618794a68f3b1222d2
Tuhin <tohin@coderex.co>
2023-02-08 19:57:20 +0600

    feat: implements axios client and interceptors

---
+++ frontend/src/axios-client.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const ACCESS_TOKEN = 'ACCESS_TOKEN';
+
+const axiosClient = axios.create({
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+});
+
+axiosClient.interceptors.request.use(config => {
+    config.headers['Authorization'] = 'Bearer ' + localStorage.getItem(ACCESS_TOKEN);
+    return config;
+});
+
+axiosClient.interceptors.response.use(response => {
+    return response;
+  }, error => {
+    if (error.response.status === 401) {
+        localStorage.removeItem(ACCESS_TOKEN);
+    }
+    return Promise.reject(error);
+});
+
+export default axiosClient;
